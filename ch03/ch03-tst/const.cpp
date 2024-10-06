cac9cf4b71060e3ff86d3b9c26f56d2263a8c731
erikbloodshed <erik.bloodshed@hotmail.com>
2024-10-06 17:38:50 +0800

    new file:   ch03/ch03-tst/const.cpp 	modified:   ch03/ch03-tst/wchar_t.cpp

---
+++ ch03/ch03-tst/const.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    const int Months{12};
+    std::cout << Months << '\n';
+    return 0;
+}
--- ch03/ch03-tst/wchar_t.cpp
+++ ch03/ch03-tst/wchar_t.cpp
@@ -3,8 +3,11 @@
 int main()
 {
     wchar_t bob = L'P';
+    auto ana = L'A';
 
     std::wcout << bob << std::endl;
     std::wcout << L"tall" << std::endl;
+    std::wcout << sizeof(ana) << std::endl;
+    std::wcout << ana << std::endl;
     return 0;
 }
